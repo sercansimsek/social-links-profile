e9c45f4111f145c5e78fb634467a02b12b0a2097
sercansimsek <sercansimsek44@gmail.com>
2025-10-29 15:25:41 +0100

    starter paack added

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export const App = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">Hello, Vite with React and Tailwind CSS!</h1>
+    </div>
+  )
+}
